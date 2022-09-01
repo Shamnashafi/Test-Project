0ba9653e0289bf47697fa00def1210ad4118f254
Shamnashafi <shamnakhani94@gmail.com>
2022-09-01 11:48:01 +0400

    Commiting my project for practicing

---
+++ Testhelloworld/src/test/java/Testhelloworld/TestPrint.java
@@ -0,0 +1,14 @@
+package Testhelloworld;
+
+public class TestPrint {
+
+	public static void main(String[] args) {
+
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		System.out.println("The sum of two numbers is :"+sum );
+
+	}
+
+}
